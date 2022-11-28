58d20f7bbabb4af1e0ddd73969ec44e8d03522d2
Shilu <89338633+shilu10@users.noreply.github.com>
2022-11-28 14:40:11 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,19 @@
+import sys
+import pyspark
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from pyspark.sql.avro.functions import *
+from pyspark.sql import SparkSession
+
+source=sys.argv[1]
+target=sys.argv[2]
+
+spark = SparkSession\
+    .builder\
+    .appName("repartition-job")\
+    .getOrCreate()
+
+rdd = spark.sparkContext.parallelize([1, 2])
+print(rdd.collect())
+
+spark.stop()
