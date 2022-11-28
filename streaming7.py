c4603bde432e86a072fdc54dcc7c8f3dfd0b56a4
Shilu <89338633+shilu10@users.noreply.github.com>
2022-11-28 18:54:23 +0530

    Update and rename streaming6.py to streaming7.py

--- streaming7.py
+++ streaming7.py
@@ -8,7 +8,7 @@ import json
 scala_version = '2.12'
 spark_version = '3.1.2'
 kafka_server = "my-cluster-kafka-bootstrap:9092"
-topic_name = "demo"
+topic_name = "demo1"
 
 packages = [
     f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
